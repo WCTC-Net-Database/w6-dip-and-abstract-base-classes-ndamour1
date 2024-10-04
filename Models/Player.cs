ce891f8f191f9b9c5b7a7bc4c584c2fd08f0b753
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-10-04 19:15:57 -0400

    Update Player.cs

--- Models/Player.cs
+++ Models/Player.cs
@@ -1,18 +1,34 @@
-﻿namespace W6_assignment_template.Models
-{
-    public class Player : CharacterBase
-    {
-        public int Gold { get; set; }
+using W6_assignment_template.Interfaces;
 
-        public Player(string name, string type, int level, int hp, int gold)
-            : base(name, type, level, hp)
+namespace W6_assignment_template.Models
+{
+    public class Player : CharacterBase, IEquipment
+    {
+        public string Equipment { get; set; }
+
+        public Player() : base() { }
+
+        public Player(string name, string type, int level, int hp, int gold, string equipment, IRoom startingRoom)
+            : base(name, type, level, hp, gold, startingRoom)
         {
-            Gold = gold;
+            Equipment = equipment;
+        }
+
+        public void Look(IRoom subject)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{Name} looks and sees {subject.Name}.");
+            Console.ResetColor();
         }
 
         public override void UniqueBehavior()
         {
             throw new NotImplementedException();
         }
+    }
+
+    public interface IEquipment
+    {
+        string Equipment { get; set; }
     }
 }
