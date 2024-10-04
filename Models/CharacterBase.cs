5e27096c98e7069ce38983b31c6285d3093a8cb3
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-10-04 19:15:22 -0400

    Update CharacterBase.cs

--- Models/CharacterBase.cs
+++ Models/CharacterBase.cs
@@ -8,27 +8,49 @@ namespace W6_assignment_template.Models
         public string Type { get; set; }
         public int Level { get; set; }
         public int HP { get; set; }
+        public int Gold { get; set; }
+        protected IRoom CurrentRoom;
 
-        protected CharacterBase(string name, string type, int level, int hp)
+        public CharacterBase() { }
+
+        protected CharacterBase(string name, string type, int level, int hp, int gold, IRoom startingRoom)
         {
             Name = name;
             Type = type;
             Level = level;
             HP = hp;
+            Gold = gold;
+            CurrentRoom = startingRoom;
         }
-
-        protected CharacterBase() { }
 
         public void Attack(ICharacter target)
         {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine($"{Name} attacks {target.Name}");
+            Console.ForegroundColor = ConsoleColor.Red;
+            if (this is Player player && player is IEquipment playerWithEquipment && !string.IsNullOrEmpty(playerWithEquipment.Equipment))
+            {
+                // Randomizer for lack of user input
+                Random rnd = new Random();
+                int equipment = rnd.Next(1, 3);
+
+                if (equipment == 1)
+                {
+                    Console.WriteLine($"{Name} attacks {target.Name} with sword.");
+                }
+                else if (equipment == 2)
+                {
+                    Console.WriteLine($"{Name} attacks {target.Name} with shield.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{Name} attacks {target.Name}.");
+            }
             Console.ResetColor();
 
-            if (this is Player player && target is ILootable targetWithTreasure && !string.IsNullOrEmpty(targetWithTreasure.Treasure))
+            if (this is Player brokePlayer && target is ILootable targetWithTreasure && !string.IsNullOrEmpty(targetWithTreasure.Treasure))
             {
                 Console.WriteLine($"{Name} takes {targetWithTreasure.Treasure} from {target.Name}");
-                player.Gold += 10; // Assuming each treasure is worth 10 gold
+                brokePlayer.Gold += 10; // Assuming each treasure is worth 10 gold
                 targetWithTreasure.Treasure = null; // Treasure is taken
             }
             else if (this is Player playerWithGold && target is Player targetWithGold && targetWithGold.Gold > 0)
@@ -42,6 +64,48 @@ namespace W6_assignment_template.Models
         public void Move()
         {
             Console.WriteLine($"{Name} moves.");
+        }
+
+        public void Enter(IRoom room)
+        {
+            CurrentRoom = room;
+        }
+
+        public virtual void MoveToRoom(string? direction = null)
+        {
+            IRoom nextRoom;
+            switch (direction)
+            {
+                case "East":
+                    nextRoom = CurrentRoom.East;
+                    break;
+                case "North":
+                    nextRoom = CurrentRoom.North;
+                    break;
+                case "South":
+                    nextRoom = CurrentRoom.South;
+                    break;
+                case "West":
+                    nextRoom = CurrentRoom.West;
+                    break;
+                default:
+                    nextRoom = null;
+                    break;
+            }
+
+            if (nextRoom != null)
+            {
+                Enter(nextRoom);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"{Name} has entered {CurrentRoom.Name}.\n{CurrentRoom.Description}");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("There is no door in that direction.");
+                Console.ResetColor();
+            }
         }
 
         // Abstract method for unique behavior to be implemented by derived classes
