6737fcc9170ac75ec432ae6db72a049a1c22ef9d
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-10-04 19:13:16 -0400

    IRoom Interface
    My IRoom interface file.

---
+++ Interfaces/IRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W6_assignment_template.Interfaces
+{
+    public interface IRoom
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IRoom North { get; set; }
+        public IRoom South { get; set; }
+        public IRoom East { get; set; }
+        public IRoom West { get; set; }
+
+        public void Enter(IRoom room);
+    }
+}
